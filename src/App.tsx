1c6f3f4405ee333edaa12776ebeb1010a3c21687
yss1235-why <cofclanone1@gmail.com>
2025-11-14 02:16:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
 import AdminJobEdit from "./pages/AdminJobEdit";
+import AdminBookings from "./pages/AdminBookings"; // NEW IMPORT
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
           <Route path="/*" element={<Index />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/admin/job/:jobId" element={<AdminJobEdit />} />
+          <Route path="/admin/bookings" element={<AdminBookings />} /> {/* NEW ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
