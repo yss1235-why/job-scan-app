fe2e9778545b015cbe438b47a3cadf07e7e0cf0e
yss1235-why <cofclanone1@gmail.com>
2025-11-14 00:42:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Admin from "./pages/Admin";
+import AdminJobEdit from "./pages/AdminJobEdit";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -14,6 +16,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<Index />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/job/:jobId" element={<AdminJobEdit />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
