e26a2f851ef0339b94b529d58abbab5c246a8791
yss1235-why <cofclanone1@gmail.com>
2025-11-14 01:15:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,9 @@ const App = () => (
       <Toaster />
       <BrowserRouter>
         <Routes>
-          {/* Admin routes MUST come before Index */}
+          <Route path="/*" element={<Index />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/admin/job/:jobId" element={<AdminJobEdit />} />
-          
-          {/* User routes */}
-          <Route path="/" element={<Index />} />
-          <Route path="/saved" element={<Index />} />
-          <Route path="/notifications" element={<Index />} />
-          <Route path="/more" element={<Index />} />
-          
-          {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
